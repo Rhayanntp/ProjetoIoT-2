f24b4d4f03acf7b376d7c8c262d35f4430671ce7
Rhayanntp <rhayanrhangel58@gmail.com>
2025-08-21 14:17:49 -0300

    first commit

---
+++ database/migrations/2025_08_12_170118_create_sensors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use SebastianBergmann\Type\FalseType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ambiente_Id')->constrained('ambientes')->onDelete('cascade');
+            $table->string('codigo')->unique();
+            $table->string('tipo')->nullable(false);
+            $table->string('descricao')->nullable(false);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensors');
+    }
+};
