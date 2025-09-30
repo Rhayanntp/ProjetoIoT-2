dc0247bdae2e5bee9f1d4f0d5b2f309c71975460
Rhayanntp <rhayanrhangel58@gmail.com>
2025-09-30 14:06:39 -0300

    Crud de ambientes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Livewire\Dashboard;
+use App\Livewire\Ambiente\AmbienteCreate;
+use App\Livewire\Ambiente\AmbienteEdit;
+use App\Livewire\Ambiente\AmbienteList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Dashboard::class);
+Route::prefix('ambiente')->group(function () {
+    Route::get('/', AmbienteList::class)->name('ambiente.list');
+    Route::get('/create', AmbienteCreate::class)->name('ambiente.create');
+    Route::get('/{id}/edit', AmbienteEdit::class)->name('ambientes.edit');
+});
